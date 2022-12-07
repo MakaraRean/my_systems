ff290f93b7d9243ccabdb473939fd2f8a7b717e3
REAN MAKARA <r.makara232@gmail.com>
2022-12-07 17:00:55 +0700

    sdfdsffsfsdf

--- resources/views/dashboard/category.blade.php
+++ resources/views/dashboard/category.blade.php
@@ -93,24 +93,11 @@
                                         <li class="paginate_button page-item previous disabled" id="dataTable_previous"><a
                                                 href="#" aria-controls="dataTable" data-dt-idx="0" tabindex="0"
                                                 class="page-link">Previous</a></li>
-                                        <li class="paginate_button page-item active"><a href="#"
-                                                aria-controls="dataTable" data-dt-idx="1" tabindex="0"
-                                                class="page-link">1</a></li>
-                                        <li class="paginate_button page-item "><a href="#"
-                                                aria-controls="dataTable" data-dt-idx="2" tabindex="0"
-                                                class="page-link">2</a></li>
-                                        <li class="paginate_button page-item "><a href="#"
-                                                aria-controls="dataTable" data-dt-idx="3" tabindex="0"
-                                                class="page-link">3</a></li>
-                                        <li class="paginate_button page-item "><a href="#"
-                                                aria-controls="dataTable" data-dt-idx="4" tabindex="0"
-                                                class="page-link">4</a></li>
-                                        <li class="paginate_button page-item "><a href="#"
-                                                aria-controls="dataTable" data-dt-idx="5" tabindex="0"
-                                                class="page-link">5</a></li>
-                                        <li class="paginate_button page-item "><a href="#"
-                                                aria-controls="dataTable" data-dt-idx="6" tabindex="0"
-                                                class="page-link">6</a></li>
+                                        @for ($i = 1; $i <= $all_page; $i++)
+                                            <li class="paginate_button page-item active"><a href="#"
+                                                    aria-controls="dataTable" data-dt-idx="{{ $i }}"
+                                                    tabindex="0" class="page-link">{{ $i }}</a></li>
+                                        @endfor
                                         <li class="paginate_button page-item next" id="dataTable_next"><a href="#"
                                                 aria-controls="dataTable" data-dt-idx="7" tabindex="0"
                                                 class="page-link">Next</a></li>
