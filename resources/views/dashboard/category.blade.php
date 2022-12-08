b9e0f4279881ae7e7d66d652ae92aa69f259a34d
REAN MAKARA <r.makara232@gmail.com>
2022-12-08 10:11:43 +0700

    ddfsf

--- resources/views/dashboard/category.blade.php
+++ resources/views/dashboard/category.blade.php
@@ -90,7 +90,7 @@
                             <div class="col-sm-12 col-md-7">
                                 <div class="dataTables_paginate paging_simple_numbers" id="dataTable_paginate">
                                     <ul class="pagination">
-                                        <li class="paginate_button page-item previous disabled" id="dataTable_previous"><a
+                                        <li class="paginate_button page-item previous disabled" id="previous"><a
                                                 href="#" aria-controls="dataTable" data-dt-idx="0" tabindex="0"
                                                 class="page-link">Previous</a></li>
                                         @for ($i = 1; $i <= $all_page; $i++)
@@ -102,9 +102,9 @@
                                                     tabindex="0" class="page-link">{{ $i }}</a>
                                             </li>
                                         @endfor
-                                        <li class="paginate_button page-item next" id="dataTable_next"><a href="#"
+                                        <li class="paginate_button page-item next" id="next"><a href="#"
                                                 aria-controls="dataTable" data-dt-idx="7" tabindex="0"
-                                                class="page-link" onclick="test()">Next</a></li>
+                                                class="page-link">Next</a></li>
                                     </ul>
                                     <button onclick="test();">test</button>
                                 </div>
@@ -117,8 +117,10 @@
     </div>
     <script src="js/category.js"></script>
     <script>
-        function test() {
-            alert('Hello');
+        function previousOnClick() {
+            var previous = document.getElementById('previous');
+            var next = document.getElementById('next');
+            alert(previous);
         }
     </script>
 @endsection
