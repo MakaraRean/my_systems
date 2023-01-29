c6f2ca003d86d800e2feaa835617c3404412a788
REAN MAKARA <r.makara232@gmail.com>
2023-01-29 18:59:06 +0700

    update

--- database/migrations/2023_01_26_044229_create_jumpeak_customers_table.php
+++ database/migrations/2023_01_26_044229_create_jumpeak_customers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('jumpeak_customers', function (Blueprint $table) {
-            $table->id('name')->is_string()->autoIncrement(false);
+            $table->string('name')->autoIncrement(false);
             $table->string('address');
             $table->string('phone')->nullable();
             $table->boolean('is_active')->default(true);
